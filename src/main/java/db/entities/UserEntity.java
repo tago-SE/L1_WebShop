2dc511f582438d3ff3b3020837fbb15dd25233d6
tago-SE <tiagor@kth.se>
2019-09-26 19:40:01 +0200

    UserEntity NamedQueries

--- src/main/java/db/entities/UserEntity.java
+++ src/main/java/db/entities/UserEntity.java
@@ -6,9 +6,11 @@ import java.io.Serializable;
 @Entity
 @Table(name = "Users")
 @NamedQueries({
+    @NamedQuery(name = "User.findAll", query = "SELECT u FROM  UserEntity u"),
     @NamedQuery(name = "User.findByName", query = "SELECT u FROM  UserEntity u WHERE u.name = :name"),
+    @NamedQuery(name = "User.findByNameContains", query = "SELECT u FROM UserEntity u WHERE u.name LIKE :search"),
     @NamedQuery(name = "User.validateCredentials", query = "SELECT u FROM  UserEntity u WHERE u.name = :name and u.password = :password")
-} )
+})
 public class UserEntity implements Serializable {
 
     @Id
